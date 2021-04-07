fe8685fdc86df7c9f1e7b7143a766745d671797d
Alex Kio <alexandermartelkio@gmail.com>
2021-04-06 21:21:16 -0400

    Move main JS and add onClick function framework

---
+++ src/main.js
@@ -0,0 +1,13 @@
+var addButton = document.querySelector('.add-recipie');
+var letsCookButton = document.querySelector('.lets-cook');
+
+addButton.addEventListener('click', addRecipie);
+letsCookButton.addEventListener('click', letsCook);
+
+function addRecipie() {
+
+};
+
+function letsCook() {
+
+};
